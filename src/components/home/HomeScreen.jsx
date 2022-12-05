3d1c995141383ff9caaf6d31160929b6b76e02b1
Peyton Moss <pmoss94@gmail.com>
2022-12-05 10:57:48 -0700

    home page fleshed out

--- src/components/home/HomeScreen.jsx
+++ src/components/home/HomeScreen.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./HomeScreen.css";
+import homeImage from "./homeImage.png"
 
 
 function HomeScreen () {
+    return (
+        <div className="home-screen"
+        style={{
+            background: `url(${homeImage})`,
+            backgroundSize: "cover",
 
+        }}>
+
+        </div>
+    )
 }
 
 export default HomeScreen;
