5c1c847a834c9c6b056aa546dde84e2b0725907d
Peyton Moss <pmoss94@gmail.com>
2023-01-26 23:05:55 -0700

    reviews added, hero buttons added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,9 +13,16 @@ function Hero() {
         backgroundPosition: "top center",
       }}
     >
-      <div className="img-div">
-        <img src={opac80} alt="" />
+      <div className="img-btns">
+        <div className="img-div">
+          <img src={opac80} alt="" />
+        </div>
+        <button className="hero-btn">Get a Quote</button>
+        <button className="hero-btn">Contact Us</button>
       </div>
+      {/* <div className="work-info-div">
+        <p className="work-info">Decks, Roofs, Pergolas, Remodeling, & More</p>
+      </div> */}
     </div>
   );
 }
--- src/components/ReviewBox.jsx
+++ src/components/ReviewBox.jsx
@@ -5,9 +5,36 @@ function ReviewBox() {
   return (
     <div className="rb-wrapper">
       <div className="rb-box">
-        <div className="box-1"></div>
-        <div className="box-2"></div>
-        <div className="box-3"></div>
+        <div className="box-1">
+          <div className="in-box-1">
+            <p className="review-p">
+              "He was awesome. We love our new deck. He did it quickly and made
+              sure it was quality work. We will recommend him to our friends."
+            </p>
+            <p className="quote-name">-Melody L.</p>
+          </div>
+        </div>
+        <div className="box-2">
+          <div className="in-box-2">
+            <p className="review-p">
+              "Joe was amazing to work with. He exceeded my expectations! Joe
+              made sure everything was perfect and communicated any changes
+              making sure we got exactly what we envisioned. We are loving our
+              new deck, and will definitely use Joe for our next home project!"
+            </p>
+            <p className="quote-name">-Neena R.</p>
+          </div>
+        </div>
+        <div className="box-3">
+          <div className="in-box-3">
+            <p className="review-p">
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus velit veritatis optio nulla libero quos illo
+              impedit, praesentium vitae harum quo accusantium natus"
+            </p>
+            <p className="quote-name">-Poopy Face</p>
+          </div>
+        </div>
       </div>
       <button className="review-btn">See More Reviews</button>
     </div>
